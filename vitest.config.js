e0167a9c48ff37f7480c9516ad8c16b7c9de9e64
eScape LLC <winker9@hotmail.com>
2023-09-03 14:35:50 -0400

    add coverage.

--- vitest.config.js
+++ vitest.config.js
@@ -8,7 +8,11 @@ export default mergeConfig(
 		test: {
 			environment: 'jsdom',
 			exclude: [...configDefaults.exclude, 'e2e/*'],
-			root: fileURLToPath(new URL('./', import.meta.url))
+			root: fileURLToPath(new URL('./', import.meta.url)),
+			coverage: {
+				provider: "v8",
+				reporter: ['text', 'json', 'html'],
+			}
 		}
 	})
 )
