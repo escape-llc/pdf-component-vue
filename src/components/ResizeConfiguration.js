9c4be1252958e8bd93a44fd169b06f3ded06cf5c
eScape LLC <winker9@hotmail.com>
2023-12-23 21:08:24 -0500

    resize dynamic; refactor.

--- src/components/ResizeConfiguration.js
+++ src/components/ResizeConfiguration.js
@@ -1,5 +1,9 @@
 import { Trigger } from "./Trigger";
 
+const DELTA_INLINE = 4;
+const DELTA_BLOCK = 4;
+const TRIGGER_TIME = 100;
+
 class ResizeConfiguration {
 	deltaInline
 	deltaBlock
@@ -25,7 +29,26 @@ class ResizeConfiguration {
 		this.triggerTime = triggerTime;
 	}
 	static defaultConfiguration() {
-		return new ResizeConfiguration(4, 4, 100);
+		return new ResizeConfiguration(DELTA_INLINE, DELTA_BLOCK, TRIGGER_TIME);
+	}
+}
+/**
+ * Use this configuration to get dynamic scaling behavior during animations and/or transitions.
+ * This only works well for small numbers of visible elements, then performance degrades.
+ * This configuration triggers additional resize behavior:
+ *   activate an InteresctionObserver to track visible elements.
+ *   after each ResizeObserver callback (before the trigger), dynamically update CSS variables of visible elements.
+ */
+class ResizeDynamicConfiguration extends ResizeConfiguration {
+	root
+	rootMargin
+	constructor(deltaInline, deltaBlock, triggerTime, root, rootMargin) {
+		super(deltaInline, deltaBlock, triggerTime);
+		this.root = root;
+		this.rootMargin = rootMargin;
+	}
+	static defaultConfiguration(root, rootMargin) {
+		return new ResizeDynamicConfiguration(DELTA_INLINE, DELTA_BLOCK, TRIGGER_TIME, root, rootMargin);
 	}
 }
 /**
@@ -51,6 +74,21 @@ class ResizeTracker extends Trigger {
 	 */
 	track(page, dpsize) {
 		this.active.set(page, dpsize);
+	}
+	/**
+	 * Calculate the delta size change.
+	 * @param {Number} key page key.
+	 * @returns {{db:Number,di:Number}|null} size delta information or NULL if not in both maps.
+	 */
+	delta(key) {
+		if(this.active.has(key) && this.lastKnown.has(key)) {
+			const value = this.active.get(key);
+			const dpsize = this.lastKnown.get(key);
+			const db = value.blockSize - dpsize.blockSize;
+			const di = value.inlineSize - dpsize.inlineSize;
+			return { db, di };
+		}
+		return null;
 	}
 	/**
 	 * Do the bookkeeping for resize handling.
@@ -96,4 +134,4 @@ class ResizeTracker extends Trigger {
 	}
 }
 
-export { ResizeConfiguration, ResizeTracker }
+export { ResizeConfiguration, ResizeDynamicConfiguration, ResizeTracker }
