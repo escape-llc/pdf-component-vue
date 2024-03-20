039a601dbe52c2db8142d0fd967e7cf0e039977a
eScape LLC <winker9@hotmail.com>
2024-03-20 10:51:58 -0400

    Use documentation update; allow viewer to be undefined.

--- src/lib/Use.js
+++ src/lib/Use.js
@@ -4,14 +4,13 @@ const pdfjsViewerSymbol = Symbol();
 /**
  * Configure PDFJS for your application.
  * @param {App} app Vue application instance.
- * @param {{pdfjs: Module, viewer: Module, workerSrc: String|URL|undefined}} options Options.
+ * @param {{pdfjs: Module, viewer: Module|undefined, workerSrc: String|URL|undefined}} options Options.
  * @param options.pdfjs The PDFJS module.
- * @param options.viewer The WEB module.
+ * @param options.viewer The WEB module or UNDEFINED to opt-out.  Opting out disables the Annotation Layer.
  * @param options.workerSrc The PDFJS worker or UNDEFINED if you set it.
  */
 function usePdfjs(app, { pdfjs, viewer, workerSrc }) {
 	if(!pdfjs) throw new Error("PDFJS is not defined");
-	if(!viewer) throw new Error("VIEWER is not defined");
 
 	if(workerSrc) {
 		pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
